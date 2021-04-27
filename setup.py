3299f5f1171cd110250f91f22f785ba60bc406be
Sefik Ilkin Serengil <serengil@gmail.com>
2021-04-27 20:44:15 +0300

    made requirements same with deepface

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.5.5',
-    install_requires=["numpy>=1.19.5", "gdown>=3.10.1", "Pillow>=5.2.0", "opencv-python>=4.5.1.48", "tensorflow>=1.9.0"]
+    install_requires=["numpy>=1.14.0", "gdown>=3.10.1", "Pillow>=5.2.0", "opencv-python>=3.4.4", "tensorflow>=1.9.0"]
 )
